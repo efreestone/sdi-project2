ff50532c44d902848a4ccb56ec849444d2025c01
Elijah Freestone <elijahfreestone@Elijahs-MacBook-Pro.local>
2012-09-05 14:45:41 -0600

    Yet another test. Soemthing isnt working

--- Freestone_Elijah_assignment2.js
+++ Freestone_Elijah_assignment2.js
@@ -1,1 +1,21 @@
-alert("JavaScript works!");
+/*
+Elijah Freestone
+SDI 1209
+Project 2
+9-4-12
+*/
+
+//Initial Variables
+var booVar = false,
+    start = "Echo and I take off from the garage in my car. ",
+    zombiesInDrive = "yes" //Procedure argument
+;
+
+console.log(start) //Start
+
+if (zombiesInDrive === "yes") { //Procedure
+        console.log("I take out several Zombies like Bowling Pins as I speed down the driveway. ") 
+        } else { console.log("No Zombies in the driveway. Looks like we're free and clear for the moment. ") }
+;
+
+// This is a commit test
